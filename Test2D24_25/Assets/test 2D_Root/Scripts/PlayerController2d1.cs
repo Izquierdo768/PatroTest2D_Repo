33c3befec4c8c737960df95bc6ac4faeb2d4af45
pablofon <153851706+pablofon@users.noreply.github.com>
2024-12-13 20:10:23 +0100

    avances 13 12
     GrounchetScript
    creación de la plataforma

--- Test2D24_25/Assets/test 2D_Root/Scripts/PlayerController2d1.cs
+++ Test2D24_25/Assets/test 2D_Root/Scripts/PlayerController2d1.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem; //Librería necesaria para leer el New Input System
 
@@ -12,10 +13,14 @@ public class PlayerController2d1 : MonoBehaviour
 
     [Header("Movement Parametres")]
     public float speed;
+    [SerializeField] bool isFacingRight;
 
     [Header("Jump Parameters")]
     public float jumpForce;
     [SerializeField] bool isGrounded;
+    [SerializeField] GameObject groundCheck;
+    [SerializeField] float groundCheckRadius;
+    [SerializeField] LayerMask groundLayer;
 
 
 
@@ -25,14 +30,22 @@ public class PlayerController2d1 : MonoBehaviour
         //Para autoreferenciar :nombre de variable=Get component de variable<()  
         playerRb = GetComponent<Rigidbody2D>();
         playerInput = GetComponent<PlayerInput>();
+        isFacingRight = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        GroundCheck();
+        if (moveInput.x > 0 && !isFacingRight) Flip();
+        if (moveInput.x < 0 && isFacingRight) Flip();
 
-    }
-
+       
+        
+    }   
+    
+    
+    
 
     private void FixedUpdate()
     
@@ -46,7 +59,19 @@ public class PlayerController2d1 : MonoBehaviour
 
         playerRb.velocity = new Vector3(moveInput.x * speed, playerRb.velocity.y, 0);
     }
+    void Flip()
+    {
+        Vector3 currentScale = transform.localScale;
+        currentScale.x *= -1;
+        transform.localScale = currentScale;
+        isFacingRight = !isFacingRight;
 
+    }
+    void GroundCheck()
+    {
+        //isGrounded es verdadero cuando el círculo detector toque la layer ground
+        isGrounded = Physics2D.OverlapCircle(groundCheck.transform.position, groundCheckRadius, groundLayer);
+    }
     #region Input Methods
 //Métodos que permiten leer el input del New Input System
 // Crearemos un método por cada acción
@@ -61,8 +86,11 @@ public class PlayerController2d1 : MonoBehaviour
     {
         if (context.started)
         {
-            
-            playerRb.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+            if (isGrounded)
+            {
+                playerRb.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+            }
+ 
         }
         
 
